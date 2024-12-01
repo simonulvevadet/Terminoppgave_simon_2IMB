71af64e8154c2fd736d5391ae4fc065f1e99da64
simonulvevadet <simonulvevadet@gmail.com>
2024-12-01 21:54:59 +0100

    redigert notater og litt html
    forandrer på notater og lagde ny knapp i html

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ app = Flask(__name__,
 
 # MySQL database connection i app.py
 db = mysql.connector.connect(
-    host="localhost",       # Standard MySQL-vertsnavn
-    user="root",            # MySQL-brukernavn (erstatt med din bruker om nødvendig)
-    password="password",    # MySQL-passord (endre til ditt passord)
+    host="localhost",       # Standard MySQL host navn
+    user="root",            # MySQL brukernavn 
+    password="password",    # MySQL passord 
     database="bike_workshop" # Navn på databasen
 )
 
